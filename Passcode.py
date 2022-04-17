8ae6478656be4b5ace97aa58d1990f26fcae1f41
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-04-16 23:20:58 -0700

    Add files via upload

---
+++ Passcode.py
@@ -0,0 +1,7 @@
+name=""
+pwd=""
+while (name=="waseem") or (pwd=="python"):
+    name=input("enter the name:")
+    pwd=input("enter the password:")
+print("Hello waseem thanks for the confirmation")    
+ 
