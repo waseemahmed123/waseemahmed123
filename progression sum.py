3f025d4dd5ce7fb6a1130488e5ba830e1b744f0f
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-04-16 22:49:36 -0700

    Add files via upload

---
+++ progression sum.py
@@ -0,0 +1,16 @@
+#calculate the sum of successive numbers
+n=int(input("enter some number:"))
+x=1
+sum=0
+while x<=n:
+   
+    sum=x+sum
+    x+=1
+    print(sum)    
+    
+    
+    
+
+
+
+    
