9c86d88eabe43125791e91986bb439d81cffc806
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-04-28 02:56:57 -0700

    Add files via upload

--- factorial_function.py
+++ factorial_function.py
@@ -1,13 +1,9 @@
-def fact(n):
-    x=1
-    while n>=1:
-        x=x*n
-        n=n-1
-    return x
-print(fact(4))
-
-
-for i in range(1,6):
-    print("factorial of {} is: {}".format(i,fact(i)))
+def sum_sub(a,b):
+    sum=a+b
+    sub=a-b
+    return sum,sub
+x,y=sum_sub(100,50)
+print("the sum:", x)
+print("the sub:", y)
         
         
