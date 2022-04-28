c2a051f99216cdb7f2719f4a27e0b7fcac4838e7
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-04-28 02:49:42 -0700

    Add files via upload

--- NestedFunc.py
+++ NestedFunc.py
@@ -1,10 +1,4 @@
-def f1():
-    def inner(a,b):
-        print("The sum:",a+b)
-        print("The avg:", (a+b)/2)
-    inner(10,20)
-    inner(20,30)
-    inner(40,50)
-    inner(100,200)
-
-f1()    
+from functools import reduce
+l=[10,20,30,40,50]
+result=reduce(lambda x,y:x+y,l)
+print(result)
